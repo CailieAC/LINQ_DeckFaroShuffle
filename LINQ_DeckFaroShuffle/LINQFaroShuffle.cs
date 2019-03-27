8e8b40e28c077f4c12cda07d545b4802ab0ca51d
CailieAC <cailiecarlile@gmail.com>
2019-03-26 22:21:33 -0500

    commit2

---
+++ LINQ_DeckFaroShuffle/LINQFaroShuffle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LINQ_DeckFaroShuffle
+{
+    //extension method is a special purpose static method that adds new 
+    //functionality to an already-existing type without having to modify 
+    //the original type you want to add functionality to.
+
+    //public static IEnumerable<T> InterleaveSequenceWith<T> (this IEnumerable<T> first, IEnumerable<T> second)
+
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+
+        public static bool SequenceEquals<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                if (!firstIter.Current.Equals(secondIter.Current))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static IEnumerable<T> LogQuery<T>(this IEnumerable<T> sequence, string tag)
+        {
+            // File.AppendText creates a new file if the file doesn't exist.
+            using (var writer = File.AppendText("debug.log"))
+            {
+                writer.WriteLine($"Executing Query {tag}");
+            }
+
+            return sequence;
+        }
+    }
+}
--- LINQ_DeckFaroShuffle/Program.cs
+++ LINQ_DeckFaroShuffle/Program.cs
@@ -1,12 +1,112 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQ_DeckFaroShuffle
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Method syntax:
+            //The compiler translates LINQ statements written with query syntax into the equivalent method call syntax
+            //var startingDeck = Suits().SelectMany(suit => Ranks().Select(rank => new { Suit = suit, Rank = rank }));
+
+            var startingDeck = from s in Suits()
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
+
+            // Display each card that we've generated and placed in startingDeck in the console
+            foreach (var card in startingDeck)
+            {
+                Console.WriteLine(card);
+            }
+
+            // 52 cards in a deck, so 52 / 2 = 26
+            var top = startingDeck.Take(26);
+            var bottom = startingDeck.Skip(26);
+            var shuffle = top.InterleaveSequenceWith(bottom);
+
+            foreach (var c in shuffle)
+            {
+                Console.WriteLine(c);
+            }
+
+            // Query for building the deck
+            // Shuffling using InterleaveSequenceWith<T>();
+            var times = 0;
+            // We can re-use the shuffle variable from earlier, or you can make a new one
+            shuffle = startingDeck;
+
+            /*
+            do
+            {
+                //shuffle = shuffle.Take(26).InterleaveSequenceWith(shuffle.Skip(26));
+                shuffle = shuffle.Skip(26).InterleaveSequenceWith(shuffle.Take(26));
+
+                foreach (var card in shuffle)
+                {
+                    Console.WriteLine(card);
+                }
+                Console.WriteLine();
+                times++;
+
+            } while (!startingDeck.SequenceEquals(shuffle));
+            */
+
+            do
+            {
+                /*
+                shuffle = shuffle.Take(26)
+                    .LogQuery("Top Half")
+                    .InterleaveSequenceWith(shuffle.Skip(26).LogQuery("Bottom Half"))
+                    .LogQuery("Shuffle")
+                    .ToArray();
+                */
+
+                shuffle = shuffle.Skip(26)
+                    .LogQuery("Bottom Half")
+                    .InterleaveSequenceWith(shuffle.Take(26).LogQuery("Top Half"))
+                    .LogQuery("Shuffle")
+                    .ToArray();
+
+                foreach (var c in shuffle)
+                {
+                    Console.WriteLine(c);
+                }
+
+                times++;
+                Console.WriteLine(times);
+            } while (!startingDeck.SequenceEquals(shuffle));
+
+            Console.WriteLine(times);
+            Console.ReadLine();
+        }
+
+        static IEnumerable<string> Suits()
+        {
+            yield return "clubs";
+            yield return "diamonds";
+            yield return "hearts";
+            yield return "spades";
+        }
+
+        static IEnumerable<string> Ranks()
+        {
+            yield return "two";
+            yield return "three";
+            yield return "four";
+            yield return "five";
+            yield return "six";
+            yield return "seven";
+            yield return "eight";
+            yield return "nine";
+            yield return "ten";
+            yield return "jack";
+            yield return "queen";
+            yield return "king";
+            yield return "ace";
         }
     }
 }
