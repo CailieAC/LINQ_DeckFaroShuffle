971927a5e30f528787ec3dd48d901b62fc084bad
CailieAC <cailiecarlile@gmail.com>
2019-03-26 21:48:52 -0500

    Add project files.

---
+++ LINQ_DeckFaroShuffle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ_DeckFaroShuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
